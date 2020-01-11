58dc0f79a2b4b80d817713a352a5f1ef7a3a6e61
Tirol_JPN <Tirol.JPN@gmail.com>
2020-01-11 22:13:25 +0900

    fix eslint

--- src/components/MyButton.vue
+++ src/components/MyButton.vue
@@ -3,10 +3,10 @@
 </template>
 
 <script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
+import { Component, Vue } from 'vue-property-decorator'
 
 @Component
-export default class Mybutton extends Vue{
-  
+export default class Mybutton extends Vue {
+
 }
 </script>
