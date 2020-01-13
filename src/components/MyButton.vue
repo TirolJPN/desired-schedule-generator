59bc4de14c9614d05bdff49d552b062a311a0094
Tirol_JPN <Tirol.JPN@gmail.com>
2020-01-13 18:45:09 +0900

    add date-picker

--- src/components/MyButton.vue
+++ src/components/MyButton.vue
@@ -1,12 +1,20 @@
 <template>
-  <button>My Button</button>
+  <VueCtkDateTimePicker v-model="ts" v-bind:range="true" v-bind:only-date="true" />
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
+import 'vue-ctk-date-time-picker/dist/vue-ctk-date-time-picker.css'
 
-@Component
+// interface HogeDatapicker{}
+const VueCtkDateTimePicker = require('vue-ctk-date-time-picker')
+
+@Component({
+  components: {
+    VueCtkDateTimePicker
+  }
+})
 export default class Mybutton extends Vue {
-
+  public ts = { 'start': '2018-04-09', 'end': '2018-04-10' }
 }
 </script>
