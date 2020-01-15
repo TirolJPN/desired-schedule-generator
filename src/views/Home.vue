3038864a804a4cd45b5261b80497dc79e7c526f1
Tirol_JPN <Tirol.JPN@gmail.com>
2020-01-15 16:04:26 +0900

    erace statements about related vuex and add data to have pair values about start and end day

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,16 @@
 <template>
   <div class="home">
-    <VueCtkDateTimePicker></VueCtkDateTimePicker>
+    <p>{{pairStartAndEndDays}}</p>
+    <!-- <VueCtkDateTimePicker v-model="hoge"></VueCtkDateTimePicker> -->
+    <VueCtkDateTimePicker v-model="pairStartAndEndDays" v-bind:range="true" v-bind:only-date="true" />
   </div>
 </template>
 
 <script lang="ts">
 // @ is an alias to /src
 import { Component, Vue } from 'vue-property-decorator'
-import VueCtkDateTimePicker from '@/components/VueCtkDateTimePicker.vue'
+import 'vue-ctk-date-time-picker/dist/vue-ctk-date-time-picker.css'
+const VueCtkDateTimePicker = require('vue-ctk-date-time-picker')
 
 @Component({
   components: {
@@ -15,5 +18,12 @@ import VueCtkDateTimePicker from '@/components/VueCtkDateTimePicker.vue'
   }
 })
 
-export default class Home extends Vue {}
+export default class Home extends Vue {
+  /**
+   * data
+   * startDay: the first day in which you are free
+   * endDay: the last day in which you are fere
+   */
+  pairStartAndEndDays:Object = {}
+}
 </script>
