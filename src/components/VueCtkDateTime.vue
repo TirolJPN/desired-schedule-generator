aebcaa0b8d01156a472a3e6d0992ad083c612707
Tirol_JPN <Tirol.JPN@gmail.com>
2020-01-16 23:44:33 +0900

    fix to use interface implementing date

--- src/components/VueCtkDateTime.vue
+++ src/components/VueCtkDateTime.vue
@@ -1,20 +1,27 @@
 <template>
+<div>
+  <p>{{ts}}</p>
   <VueCtkDateTimePicker v-model="ts" v-bind:range="true" v-bind:only-date="true" />
+</div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
 import 'vue-ctk-date-time-picker/dist/vue-ctk-date-time-picker.css'
-
-// interface HogeDatapicker{}
 const VueCtkDateTimePicker = require('vue-ctk-date-time-picker')
+
+interface HogeDatapicker{
+  start: string,
+  end: string
+}
 
 @Component({
   components: {
     VueCtkDateTimePicker
   }
 })
-export default class Mybutton extends Vue {
-  public ts = { 'start': '2019-04-09', 'end': '2019-04-10' }
+
+export default class VueCtkDateTime extends Vue {
+  ts:HogeDatapicker = { start: '2019-04-09', end: '2019-04-30' }
 }
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,28 +1,25 @@
 <template>
   <div class="home">
-    <p>{{pairStartAndEndDays}}</p>
-    <!-- <VueCtkDateTimePicker v-model="hoge"></VueCtkDateTimePicker> -->
-    <VueCtkDateTimePicker v-model="pairStartAndEndDays" v-bind:range="true" v-bind:only-date="true" />
+    <!-- <p>{{pairStartAndEndDays}}</p> -->
+    <VueCtkDateTime v-model="pairStartAndEndDays" v-bind:range="true" v-bind:only-date="true" />
   </div>
 </template>
 
 <script lang="ts">
 // @ is an alias to /src
 import { Component, Vue } from 'vue-property-decorator'
-import 'vue-ctk-date-time-picker/dist/vue-ctk-date-time-picker.css'
-const VueCtkDateTimePicker = require('vue-ctk-date-time-picker')
+import VueCtkDateTime from '@/components/VueCtkDateTime.vue'
 
 @Component({
   components: {
-    VueCtkDateTimePicker
+    VueCtkDateTime
   }
 })
 
 export default class Home extends Vue {
   /**
    * data
-   * startDay: the first day in which you are free
-   * endDay: the last day in which you are fere
+   * pairSrartAndEndDays: A pair of first and end candidate dates
    */
   pairStartAndEndDays:Object = {}
 }
