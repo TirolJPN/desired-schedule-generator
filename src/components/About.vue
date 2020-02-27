1802e43435523cffcc3898d49d1847bab1bc5571
Tirol_JPN <Tirol.JPN@gmail.com>
2020-02-27 22:47:13 +0900

    fix explain

--- src/components/About.vue
+++ src/components/About.vue
@@ -6,10 +6,10 @@
           <slot/>
         </div>
         <h2>
-          Desired scheduleは、空いた予定の一覧を簡単に取得できます
+          Desired scheduleは、空いた予定の一覧を示す文字列を簡単に取得できます
         </h2>
         <p>日付を入力することで、その日が空いた日であることを示します</p>
-        <p>時間が正確に入力されていないか、空欄になっている日については終日空いているものとなります</p>
+        <p>時間が空欄になっている日については終日空いているものとして扱います</p>
         <p>「copy to clipboard」をクリップボードにコピーできます</p>
         <button class="times-button" @click="$emit('close')" >
           <i class="fas fa-times"></i>
